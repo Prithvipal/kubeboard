465d837b41a993b42e1bfa9da685f04bd3a9664e
Prithvipal Singh <prithvirathore99@gmail.com>
2021-01-12 21:39:01 +0530

    list of pods

---
+++ main.js
@@ -0,0 +1,24 @@
+const electron = require('electron');
+const url = require('url')
+const path = require('path')
+
+const {app, BrowserWindow} = electron;
+
+let mainWindow;
+
+app.on('ready', function(){
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    })
+    mainWindow.loadURL(
+        url.format(
+            {
+                pathname: path.join(__dirname, 'mainWindow.html'),
+                protocol: 'file:',
+                slashes: true        
+            }
+        )
+    );
+});
