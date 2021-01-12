0fb84ad1546e0c54948039f30caa17682a3d4d6e
Prithvipal Singh <prithvirathore99@gmail.com>
2021-01-12 21:04:07 +0530

    temp code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import k8s from  '@kubernetes/client-node'
+//import k8s from  '@kubernetes/client-node'
+const k8s = require('@kubernetes/client-node');
 
 class App extends React.Component{
     render(){
+       const kc = new k8s.KubeConfig();
+        kc.loadFromDefault();
+       const k8sApi = kc.makeApiClient(k8s.CoreV1Api)
+        k8sApi.listNamespacedPod('default').then((res) => {
+            console.log(res.body);
+        });
         return <div>dasfasdf</div>
     }
 }
